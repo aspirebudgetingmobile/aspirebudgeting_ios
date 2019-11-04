5cf32e3fbac6e24259a358f62d641f6e3ea8c8aa
Mohit Athwani <mohit@athwani.net>
2019-11-03 16:04:48 -0800

    Added test for fetching Dashboard data

---
+++ Aspire BudgetingTests/MockFile.swift
@@ -0,0 +1,15 @@
+//
+//  MockFile.swift
+//  Aspire BudgetingTests
+//
+//  Created by TeraMo Labs on 11/3/19.
+//  Copyright © 2019 TeraMo Labs. All rights reserved.
+//
+
+@testable import Aspire_Budgeting
+import Foundation
+
+struct MockFile: AspireFile {
+  var name: String?
+  var identifier: String?
+}
