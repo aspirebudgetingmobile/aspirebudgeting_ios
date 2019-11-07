ca75dcb9c82950e6e27c02eae72bc88f78473ae5
Mohit Athwani <mohit@athwani.net>
2019-11-06 19:44:48 -0800

    Sign in screen with Video and helper text

---
+++ Aspire Budgeting/Views/YoutubePlayerView.swift
@@ -0,0 +1,30 @@
+//
+//  YoutubePlayerView.swift
+//  Aspire Budgeting
+//
+//  Created by TeraMo Labs on 11/6/19.
+//  Copyright © 2019 TeraMo Labs. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct YoutubePlayerView: UIViewRepresentable{
+
+  func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<YoutubePlayerView>) {
+    
+  }
+  
+  func makeUIView(context: UIViewRepresentableContext<YoutubePlayerView>) -> WKWebView {
+    let webConfiguration = WKWebViewConfiguration()
+    webConfiguration.allowsInlineMediaPlayback = false
+    
+    let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+    
+    let videoURL = URL(string: "https://www.youtube.com/embed/RBf9YBBDgbs")
+    let request = URLRequest(url: videoURL!)
+    webView.load(request)
+    
+    return webView
+  }
+}
