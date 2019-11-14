ed3f188105b0c2662fbcbc51f0bcec353c286d1a
Mohit Athwani <mohit@athwani.net>
2019-11-13 19:12:00 -0800

    FaceID for active states.

--- Aspire Budgeting/SceneDelegate.swift
+++ Aspire Budgeting/SceneDelegate.swift
@@ -88,11 +88,16 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func sceneDidBecomeActive(_ scene: UIScene) {
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    if stateManager.currentState == .needsLocalAuthentication ||
+      stateManager.currentState == .localAuthFailed {
+      userManager.authenticateLocally()
+    }
   }
 
   func sceneWillResignActive(_ scene: UIScene) {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
+    NotificationCenter.default.post(name: Notification.Name("background"), object: nil, userInfo: nil)
   }
 
   func sceneWillEnterForeground(_ scene: UIScene) {
