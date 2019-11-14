813a490a6507c27ac5a99312dff8095f6092cb3b
Mohit Athwani <mohit@athwani.net>
2019-11-13 18:00:00 -0800

    Trying to set up the new Home screen

--- Aspire Budgeting/Views/SignInView.swift
+++ Aspire Budgeting/Views/SignInView.swift
@@ -11,16 +11,41 @@ import SwiftUI
 struct SignInView: View {
     var body: some View {
         VStack {
-          Image("logo").resizable().aspectRatio(contentMode: .fit)
-          Text("Aspire").font(.custom("Nunito-Regular", size: 50)).foregroundColor(.white)
-          YoutubePlayerView().padding()
-          Spacer()
-          Text("Link your Google Account").font(.title).foregroundColor(.white)
-          Text("Why? Aspire Budgeting requires access to your Google Sheets in order to be able to display your budgeting sheet.").foregroundColor(.white).font(.footnote).padding(.horizontal)
-          GoogleSignInButton().frame(height: 50).padding()
+          Group {
+            Image("logo").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150)
+            Text("Aspire").font(.custom("Nunito-Regular", size: 30)).foregroundColor(.white).padding(-20)
+          }
+          Divider().background(Color.white).padding(.horizontal, 20)
+          Group {
+            Text("Link your Google Account").font(.body).foregroundColor(.white)
+            Text("Aspire Budgeting requires you to either link your existing budget sheet or create a new budget sheet on your Google Drive").font(.caption).foregroundColor(.white).padding(10).multilineTextAlignment(.center)
+          }
           
-          Spacer()
-        }.background(BackgroundColorView().edgesIgnoringSafeArea(.all))
+          YoutubePlayerView().padding()
+          Text("Step 1")
+//          Spacer()
+//          Text("Link your Google Account").font(.title).foregroundColor(.white)
+//          Text("Why? Aspire Budgeting requires access to your Google Sheets in order to be able to display your budgeting sheet.").foregroundColor(.white).font(.footnote).padding(.horizontal)
+          GoogleSignInButton().frame(height: 50).padding()
+          Text("Step 2")
+          GoogleSignInButton().frame(height: 50).padding()
+//          HStack {
+//            VStack {
+//              Divider().padding(20)
+//            }
+//
+//            Text("OR")
+//
+//            VStack {
+//              Divider().padding(20)
+//            }
+//          }
+//          Group {
+//            GoogleSignInButton().frame(height: 50).padding()
+//            Text("Aspire Budgeting requires you to either link your existing budget sheet or create a new budget sheet on your Google Drive").font(.caption).padding(10).multilineTextAlignment(.center)
+//          }
+          
+        }.background(BackgroundSplitColorView().edgesIgnoringSafeArea(.all))
     }
 }
 
