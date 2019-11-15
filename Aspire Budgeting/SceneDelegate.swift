68ccace14b76536ad11a15fede2c8958eafb504a
Mohit Athwani <mohit.athwani@gmail.com>
2019-11-14 18:57:48 -0800

    FaceID foreground fix (#47)
    * Now will ask for FaceID when entering foreground
    
    * FaceID for active states.

--- Aspire Budgeting/SceneDelegate.swift
+++ Aspire Budgeting/SceneDelegate.swift
@@ -88,23 +88,31 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func sceneDidBecomeActive(_ scene: UIScene) {
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    if stateManager.currentState == .needsLocalAuthentication ||
+      stateManager.currentState == .localAuthFailed {
+      userManager.authenticateLocally()
+    }
   }
 
   func sceneWillResignActive(_ scene: UIScene) {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
+    NotificationCenter.default.post(name: Notification.Name("background"), object: nil, userInfo: nil)
   }
 
   func sceneWillEnterForeground(_ scene: UIScene) {
     // Called as the scene transitions from the background to the foreground.
     // Use this method to undo the changes made on entering the background.
+    if stateManager.currentState == .needsLocalAuthentication ||
+      stateManager.currentState == .localAuthFailed {
+      userManager.authenticateLocally()
+    }
   }
 
   func sceneDidEnterBackground(_ scene: UIScene) {
     // Called as the scene transitions from the foreground to the background.
     // Use this method to save data, release shared resources, and store enough scene-specific state information
     // to restore the scene back to its current state.
-    
     NotificationCenter.default.post(name: Notification.Name("background"), object: nil, userInfo: nil)
   }
 
