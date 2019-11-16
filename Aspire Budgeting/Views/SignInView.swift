de3298ab97acf85dda065396b7c86f43aeed14f4
Mohit Athwani <mohit@athwani.net>
2019-11-15 19:38:31 -0800

    New signinView looking good

--- Aspire Budgeting/Views/SignInView.swift
+++ Aspire Budgeting/Views/SignInView.swift
@@ -9,48 +9,52 @@
 import SwiftUI
 
 struct SignInView: View {
-    var body: some View {
+  var body: some View {
+    VStack {
+      
+      Group {
+        Image("logo").resizable().aspectRatio(contentMode: .fit).frame(minHeight: 20)
+        Text("Aspire").font(.custom("Nunito-Regular", size: 30)).foregroundColor(.white).padding(-20)
+        Divider().background(Color.white).padding(.horizontal, 20)
+      }
+      
+      Group {
+        Text("Link your Google Account").font(.body).foregroundColor(.white)
+        Text("Aspire Budgeting requires you to either link your existing budget sheet or create a new budget sheet in your Google Drive").font(.caption).foregroundColor(.white).padding(10).multilineTextAlignment(.center)
+      }
+      
+      YoutubePlayerView().padding(.horizontal).frame(minHeight: 80)
+      
+      Group {
+        Text("Step 1")
+        GoogleSignInButton().frame(height: 50).padding(.horizontal)
+      }
+      
+      Text("Step 2")
+      GoogleSignInButton().frame(height: 50).padding(.horizontal)
+      
+      HStack {
         VStack {
-          Group {
-            Image("logo").resizable().aspectRatio(contentMode: .fit).frame(width: 150, height: 150)
-            Text("Aspire").font(.custom("Nunito-Regular", size: 30)).foregroundColor(.white).padding(-20)
-          }
-          Divider().background(Color.white).padding(.horizontal, 20)
-          Group {
-            Text("Link your Google Account").font(.body).foregroundColor(.white)
-            Text("Aspire Budgeting requires you to either link your existing budget sheet or create a new budget sheet on your Google Drive").font(.caption).foregroundColor(.white).padding(10).multilineTextAlignment(.center)
-          }
-          
-          YoutubePlayerView().padding()
-          Text("Step 1")
-//          Spacer()
-//          Text("Link your Google Account").font(.title).foregroundColor(.white)
-//          Text("Why? Aspire Budgeting requires access to your Google Sheets in order to be able to display your budgeting sheet.").foregroundColor(.white).font(.footnote).padding(.horizontal)
-          GoogleSignInButton().frame(height: 50).padding()
-          Text("Step 2")
-          GoogleSignInButton().frame(height: 50).padding()
-//          HStack {
-//            VStack {
-//              Divider().padding(20)
-//            }
-//
-//            Text("OR")
-//
-//            VStack {
-//              Divider().padding(20)
-//            }
-//          }
-//          Group {
-//            GoogleSignInButton().frame(height: 50).padding()
-//            Text("Aspire Budgeting requires you to either link your existing budget sheet or create a new budget sheet on your Google Drive").font(.caption).padding(10).multilineTextAlignment(.center)
-//          }
-          
-        }.background(BackgroundSplitColorView().edgesIgnoringSafeArea(.all))
-    }
+          Divider().padding(.horizontal, 20)
+        }
+        
+        Text("OR")
+        
+        VStack {
+          Divider().padding(.horizontal, 20)
+        }
+      }
+      GoogleSignInButton().frame(height: 50).padding(.horizontal)
+      Button("Privacy Policy", action: {
+        
+      })
+      Spacer()
+    }.background(BackgroundSplitColorView().edgesIgnoringSafeArea(.all))
+  }
 }
 
 struct SignInView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignInView()
-    }
+  static var previews: some View {
+    SignInView()
+  }
 }
