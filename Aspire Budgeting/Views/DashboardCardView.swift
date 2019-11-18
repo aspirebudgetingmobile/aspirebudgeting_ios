f090cc8a2b1881ab04f52556f9f6ec52507ac763
Mohit Athwani <mohit@athwani.net>
2019-11-17 17:33:03 -0800

    Adding gradient to the available text

--- Aspire Budgeting/Views/DashboardCardView.swift
+++ Aspire Budgeting/Views/DashboardCardView.swift
@@ -16,24 +16,24 @@ struct DashboardCardView: View {
   
   let categoryName: String
   let totals: Totals
-  
-    var body: some View {
-        HStack {
-          VStack(alignment: .leading) {
-            Text(categoryName).font(.title).padding([.top])
-            Text("Budgeted").font(.caption).padding([.top])
-            Text(totals.budgetedTotal).font(.headline).padding([.top], 5).padding([.bottom])
-          }.padding([.horizontal])
-          Spacer()
-          VStack {
-            Text(totals.availableTotal).font(.largeTitle).padding([.trailing])
-            Text("Available").font(.caption)
-          }
-          }.background(Color.gray)
-          .cornerRadius(10)
-        .shadow(radius: 5)
-          .padding()
-    }
+  let lg = LinearGradient(gradient: Gradient(colors: [Color(red: 95/255, green: 224/255, blue: 170/255), Color(red: 32/255, green: 172/255, blue: 122/255)]), startPoint: .top, endPoint: .bottom)
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading) {
+        Text(categoryName).font(.title).padding([.top])
+        Text("Budgeted").font(.caption).padding([.top])
+        Text(totals.budgetedTotal).font(.headline).padding([.top], 5).padding([.bottom])
+      }.padding([.horizontal])
+      Spacer()
+      VStack {
+        Text(totals.availableTotal).font(.largeTitle).foregroundColor(.clear).padding([.trailing]).overlay(lg.mask(Text(totals.availableTotal).font(.largeTitle).scaledToFill()))
+        Text("Available").font(.caption)
+      }
+    }.background(Color.gray)
+      .cornerRadius(10)
+      .shadow(radius: 5)
+      .padding()
+  }
 }
 
 //struct DashboardCardVIew_Previews: PreviewProvider {
