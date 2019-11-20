f397d8f930b9e02fc6688c08354c82e29bb9bdd3
Mohit Athwani <mohit@athwani.net>
2019-11-19 20:52:47 -0800

    Added colors to the card view

--- Aspire Budgeting/Views/DashboardCardView.swift
+++ Aspire Budgeting/Views/DashboardCardView.swift
@@ -20,16 +20,16 @@ struct DashboardCardView: View {
   var body: some View {
     HStack {
       VStack(alignment: .leading) {
-        Text(categoryName).font(.custom("Rubik-Regular", size: 20)).padding([.top]).foregroundColor(.white)
-        Text("Budgeted").font(.custom("Rubik-Light", size: 13)).padding([.top]).foregroundColor(.white)
-        Text(totals.budgetedTotal).font(.custom("Rubik-Medium", size: 15)).padding([.top], 5).padding([.bottom]).foregroundColor(.white)
+        Text(categoryName).tracking(1).font(.custom("Rubik-Regular", size: 20)).padding([.top]).foregroundColor(.white)
+        Text("Budgeted").tracking(1).font(.custom("Rubik-Light", size: 13)).padding([.top]).foregroundColor(.white)
+        Text(totals.budgetedTotal).tracking(1.18).font(.custom("Rubik-Medium", size: 15)).padding([.top], 5).padding([.bottom]).foregroundColor(.clear).overlay(Colors.redGradient.mask(Text(totals.budgetedTotal).tracking(1.18).font(.custom("Rubik-Medium", size: 15)).scaledToFill()))
       }.padding([.horizontal])
       
       Spacer()
       
       VStack {
-        Text(totals.availableTotal).font(.largeTitle).foregroundColor(.clear).padding([.trailing]).overlay(Colors.greenGradient.mask(Text(totals.availableTotal).font(.custom("Rubik-Medium", size: 30)).scaledToFill()))
-        Text("Available").font(.custom("Rubik-Light", size: 13)).foregroundColor(.white)
+        Text(totals.availableTotal).tracking(2.34).font(.custom("Rubik-Medium", size: 30)).foregroundColor(.clear).padding([.trailing]).overlay(Colors.greenGradient.mask(Text(totals.availableTotal).tracking(2.34).font(.custom("Rubik-Medium", size: 30)).scaledToFill()))
+        Text("Available").tracking(1).font(.custom("Rubik-Light", size: 13)).foregroundColor(.white)
       }
     }.background(Color.white.opacity(0.07))
       .cornerRadius(10)
