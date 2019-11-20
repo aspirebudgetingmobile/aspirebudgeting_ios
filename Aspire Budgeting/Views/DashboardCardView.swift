d8bd77b57049f7f34d10309c4c387ad7b4f843f5
Mohit Athwani <mohit@athwani.net>
2019-11-19 20:17:09 -0800

    Added the colors and fonts to the Card View

--- Aspire Budgeting/Views/DashboardCardView.swift
+++ Aspire Budgeting/Views/DashboardCardView.swift
@@ -20,18 +20,18 @@ struct DashboardCardView: View {
   var body: some View {
     HStack {
       VStack(alignment: .leading) {
-        Text(categoryName).font(.title).padding([.top])
-        Text("Budgeted").font(.caption).padding([.top])
-        Text(totals.budgetedTotal).font(.headline).padding([.top], 5).padding([.bottom])
+        Text(categoryName).font(.custom("Rubik-Regular", size: 20)).padding([.top]).foregroundColor(.white)
+        Text("Budgeted").font(.custom("Rubik-Light", size: 13)).padding([.top]).foregroundColor(.white)
+        Text(totals.budgetedTotal).font(.custom("Rubik-Medium", size: 15)).padding([.top], 5).padding([.bottom]).foregroundColor(.white)
       }.padding([.horizontal])
       
       Spacer()
       
       VStack {
-        Text(totals.availableTotal).font(.largeTitle).foregroundColor(.clear).padding([.trailing]).overlay(Colors.greenGradient.mask(Text(totals.availableTotal).font(.largeTitle).scaledToFill()))
-        Text("Available").font(.caption)
+        Text(totals.availableTotal).font(.largeTitle).foregroundColor(.clear).padding([.trailing]).overlay(Colors.greenGradient.mask(Text(totals.availableTotal).font(.custom("Rubik-Medium", size: 30)).scaledToFill()))
+        Text("Available").font(.custom("Rubik-Light", size: 13)).foregroundColor(.white)
       }
-    }.background(Color.gray.opacity(0.2))
+    }.background(Color.white.opacity(0.07))
       .cornerRadius(10)
       .shadow(radius: 5)
       .padding()
