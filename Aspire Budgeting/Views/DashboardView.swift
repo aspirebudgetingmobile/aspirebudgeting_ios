b3e28526abea403d8060affe904b98af9ec935c8
Mohit Athwani <mohit@athwani.net>
2019-11-19 20:03:47 -0800

    Hiding the nav bar in Dashboard view

--- Aspire Budgeting/Views/DashboardView.swift
+++ Aspire Budgeting/Views/DashboardView.swift
@@ -36,7 +36,10 @@ struct DashboardView: View {
       } else {
         ErrorBannerView(error: self.sheetsManager.error!)
       }
-    }.navigationBarTitle("Dashboard").background(Colors.aspireGray).edgesIgnoringSafeArea(.all).onAppear {
+    }.navigationBarHidden(true)
+      .navigationBarBackButtonHidden(true)
+      .background(Colors.aspireGray)
+      .edgesIgnoringSafeArea(.all).onAppear {
 
       self.sheetsManager.verifySheet(spreadsheet: self.file)
       self.sheetsManager.fetchCategoriesAndGroups(spreadsheet: self.file)
