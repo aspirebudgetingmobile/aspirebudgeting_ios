51fd3e8196601013a4a665bcc0919c5ae73e51bb
Mohit Athwani <mohit@athwani.net>
2019-12-08 15:36:04 -0800

    Implementing a basic form

--- Aspire Budgeting/Views/AddTransactionView.swift
+++ Aspire Budgeting/Views/AddTransactionView.swift
@@ -9,13 +9,35 @@
 import SwiftUI
 
 struct AddTransactionView: View {
+  @EnvironmentObject var sheetsManager: GoogleSheetsManager
+  
+  @State private var amountString = ""
+  @State private var date = Date()
+  @State private var category = ""
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+      Form {
+        Section(header: Text("")) {
+          TextField("Enter Amount", text: $amountString).keyboardType(.decimalPad).textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        Section(header: Text("")) {
+          DatePicker(selection: $date, in: ...Date(), displayedComponents: .date) {
+            Text("Enter Date")
+            }.foregroundColor(.white)
+        }
+        Section(header: Text("")) {
+          Picker(selection: $category, label: Text("Select Category")) {
+            ForEach(0 ..< self.sheetsManager.transactionCategories!.count) {
+              Text(self.sheetsManager.transactionCategories![$0])
+            }
+          }.foregroundColor(.white)
+        }
+      }.background(Colors.aspireGray).edgesIgnoringSafeArea(.all)
+  }
 }
 
-struct AddTransactionView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddTransactionView()
-    }
-}
+//struct AddTransactionView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AddTransactionView()
+//    }
+//}
