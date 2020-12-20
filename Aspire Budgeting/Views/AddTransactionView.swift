c5140eadfc97fb4cbb3ba44d3f971edb2ecbd7db
Mohit Athwani <mohit@athwani.net>
2020-12-20 13:06:41 -0800

    AddTransactions base with banners and scroll view ready

--- Aspire Budgeting/Views/AddTransactionView.swift
+++ Aspire Budgeting/Views/AddTransactionView.swift
@@ -6,169 +6,193 @@
 import SwiftUI
 
 struct AddTransactionView: View {
-  @EnvironmentObject var sheetsManager: GoogleSheetsManager
+//  @EnvironmentObject var sheetsManager: GoogleSheetsManager
 
-  var dateFormatter: DateFormatter {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .long
-    return formatter
-  }
+//  var dateFormatter: DateFormatter {
+//    let formatter = DateFormatter()
+//    formatter.dateStyle = .long
+//    return formatter
+//  }
 
-  @State private var amountString = ""
-  @State private var memoString = ""
-
-  @State private var showDatePicker = false
-  @State private var selectedDate = Date()
-  @State private var dateSelected = false
-
-  @State private var showCategoriesPicker = false
-  @State private var categorySelected = false
-  @State private var selectedCategory = 0
-
-  @State private var showAccountPicker = false
-  @State private var accountSelected = false
-  @State private var selectedAccount = 0
-
-  @State private var transactionType = -1
-  @State private var approvalType = -1
-
-  @State private var showingAlert = false
-  @State private var transactionAdded = false
-
-  func getDateString() -> String {
-    self.dateSelected ? self.dateFormatter.string(from: self.selectedDate) : "Select Date"
-  }
-
-  func getSelectedCategory() -> String {
-    self.categorySelected
-      ? self.sheetsManager.transactionCategories![self.selectedCategory]
-      : "Select Category"
-  }
-
-  func getSelectedAccount() -> String {
-    self.accountSelected
-      ? self.sheetsManager.transactionAccounts![self.selectedAccount]
-      : "Select Account"
-  }
-
-  func clearInputs() {
-    self.amountString = ""
-    self.memoString = ""
-    self.dateSelected = false
-    self.categorySelected = false
-    self.accountSelected = false
-    self.transactionType = -1
-    self.approvalType = -1
-  }
+//  @State private var amountString = ""
+//  @State private var memoString = ""
+//
+//  @State private var showDatePicker = false
+//  @State private var selectedDate = Date()
+//  @State private var dateSelected = false
+//
+//  @State private var showCategoriesPicker = false
+//  @State private var categorySelected = false
+//  @State private var selectedCategory = 0
+//
+//  @State private var showAccountPicker = false
+//  @State private var accountSelected = false
+//  @State private var selectedAccount = 0
+//
+//  @State private var transactionType = -1
+//  @State private var approvalType = -1
+//
+//  @State private var showingAlert = false
+//  @State private var transactionAdded = false
+//
+//  func getDateString() -> String {
+//    self.dateSelected ? self.dateFormatter.string(from: self.selectedDate) : "Select Date"
+//  }
+//
+//  func getSelectedCategory() -> String {
+//    self.categorySelected
+//      ? self.sheetsManager.transactionCategories![self.selectedCategory]
+//      : "Select Category"
+//  }
+//
+//  func getSelectedAccount() -> String {
+//    self.accountSelected
+//      ? self.sheetsManager.transactionAccounts![self.selectedAccount]
+//      : "Select Account"
+//  }
+//
+//  func clearInputs() {
+//    self.amountString = ""
+//    self.memoString = ""
+//    self.dateSelected = false
+//    self.categorySelected = false
+//    self.accountSelected = false
+//    self.transactionType = -1
+//    self.approvalType = -1
+//  }
 
   var body: some View {
-    ScrollView {
-      Group {
-        AspireTextField(
-          text: $amountString,
-          placeHolder: "Enter Amount",
-          imageName: "dollar_icon",
-          keyboardType: .decimalPad
-        )
-
-        AspireTextField(
-          text: $memoString,
-          placeHolder: "Add Memo",
-          imageName: "memo_icon",
-          keyboardType: .default
-        )
+    VStack {
+      BannerView(baseColor: .materialLightBlue800) {
+        Text("Enter Trasnsaction")
+          .bannerTitle()
       }
 
-      AspirePickerButton(title: getDateString(), imageName: "calendar_icon") {
-        withAnimation {
-          self.dateSelected = true
-          self.showDatePicker.toggle()
-        }
-      }
-      if showDatePicker {
-        DatePicker(selection: $selectedDate, in: ...Date(), displayedComponents: .date) {
-          Text("")
-        }.foregroundColor(Color.white)
-      }
-      AspirePickerButton(title: getSelectedCategory(), imageName: "categories_icon") {
-        withAnimation {
-          self.categorySelected = true
-          self.showCategoriesPicker.toggle()
-        }
-      }.disabled(self.sheetsManager.transactionCategories == nil)
-      if showCategoriesPicker {
-        Picker(selection: $selectedCategory, label: Text("")) {
-          ForEach(0..<self.sheetsManager.transactionCategories!.count) {
-            Text(self.sheetsManager.transactionCategories![$0])
-              .foregroundColor(.white)
-          }
-        }
-      }
+      BannerView(baseColor: .materialBlue800) {
+        Text("$500")
+          .padding(.top, -30)
+          .bannerTitle()
 
-      AspirePickerButton(title: getSelectedAccount(), imageName: "accounts_icon") {
-        withAnimation {
-          self.accountSelected = true
-          self.showAccountPicker.toggle()
-        }
-      }.disabled(self.sheetsManager.transactionAccounts == nil)
-      if showAccountPicker {
-        Picker(selection: $selectedAccount, label: Text("")) {
-          ForEach(0..<self.sheetsManager.transactionAccounts!.count) {
-            Text(self.sheetsManager.transactionAccounts![$0])
-              .foregroundColor(.white)
-          }
-        }
       }
+      .cornerRadius(24)
+      .shadow(radius: 5)
+      .padding(.top, -35)
 
-      AspireRadioControl(
-        selectedOption: $transactionType,
-        firstOption: "Inflow",
-        secondOption: "Outflow"
-      )
+      ScrollView {
 
-      AspireRadioControl(
-        selectedOption: $approvalType,
-        firstOption: "Approved",
-        secondOption: "Pending"
-      )
-
-      AspireButton(title: "Add", type: .green) {
-        if !self.amountString.isEmpty,
-          self.dateSelected,
-          self.categorySelected,
-          self.accountSelected {
-          self.sheetsManager.addTransaction(
-            amount: self.amountString,
-            memo: self.memoString,
-            date: self.selectedDate,
-            category: self.selectedCategory,
-            account: self.selectedAccount,
-            transactionType: self.transactionType,
-            approvalType: self.approvalType
-          ) { result in
-            self.clearInputs()
-            self.transactionAdded = result
-            self.showingAlert = true
-          }
-        }
-      }.padding().alert(isPresented: $showingAlert) { () -> Alert in
-        if self.transactionAdded {
-          return Alert(title: Text("Transaction added"))
-        } else {
-          return Alert(title: Text("An error occured. Please try again."))
-        }
-      }
-    }.background(Colors.aspireGray)
+      }.background(Color.primaryBackgroundColor)
+      .cornerRadius(24)
+      .shadow(radius: 5)
+      .padding(.top, -50)
       .edgesIgnoringSafeArea(.all)
-      .onTapGesture {
-        UIApplication.shared.sendAction(
-          #selector(UIResponder.resignFirstResponder),
-          to: nil,
-          from:
-          nil,
-          for: nil
-        )
-      }
+    }
+//    ScrollView {
+//      Group {
+//        AspireTextField(
+//          text: $amountString,
+//          placeHolder: "Enter Amount",
+//          imageName: "dollar_icon",
+//          keyboardType: .decimalPad
+//        )
+//
+//        AspireTextField(
+//          text: $memoString,
+//          placeHolder: "Add Memo",
+//          imageName: "memo_icon",
+//          keyboardType: .default
+//        )
+//      }
+//
+//      AspirePickerButton(title: getDateString(), imageName: "calendar_icon") {
+//        withAnimation {
+//          self.dateSelected = true
+//          self.showDatePicker.toggle()
+//        }
+//      }
+//      if showDatePicker {
+//        DatePicker(selection: $selectedDate, in: ...Date(), displayedComponents: .date) {
+//          Text("")
+//        }.foregroundColor(Color.white)
+//      }
+//      AspirePickerButton(title: getSelectedCategory(), imageName: "categories_icon") {
+//        withAnimation {
+//          self.categorySelected = true
+//          self.showCategoriesPicker.toggle()
+//        }
+//      }.disabled(self.sheetsManager.transactionCategories == nil)
+//      if showCategoriesPicker {
+//        Picker(selection: $selectedCategory, label: Text("")) {
+//          ForEach(0..<self.sheetsManager.transactionCategories!.count) {
+//            Text(self.sheetsManager.transactionCategories![$0])
+//              .foregroundColor(.white)
+//          }
+//        }
+//      }
+//
+//      AspirePickerButton(title: getSelectedAccount(), imageName: "accounts_icon") {
+//        withAnimation {
+//          self.accountSelected = true
+//          self.showAccountPicker.toggle()
+//        }
+//      }.disabled(self.sheetsManager.transactionAccounts == nil)
+//      if showAccountPicker {
+//        Picker(selection: $selectedAccount, label: Text("")) {
+//          ForEach(0..<self.sheetsManager.transactionAccounts!.count) {
+//            Text(self.sheetsManager.transactionAccounts![$0])
+//              .foregroundColor(.white)
+//          }
+//        }
+//      }
+//
+//      AspireRadioControl(
+//        selectedOption: $transactionType,
+//        firstOption: "Inflow",
+//        secondOption: "Outflow"
+//      )
+//
+//      AspireRadioControl(
+//        selectedOption: $approvalType,
+//        firstOption: "Approved",
+//        secondOption: "Pending"
+//      )
+//
+//      AspireButton(title: "Add", type: .green) {
+//        if !self.amountString.isEmpty,
+//          self.dateSelected,
+//          self.categorySelected,
+//          self.accountSelected {
+//          self.sheetsManager.addTransaction(
+//            amount: self.amountString,
+//            memo: self.memoString,
+//            date: self.selectedDate,
+//            category: self.selectedCategory,
+//            account: self.selectedAccount,
+//            transactionType: self.transactionType,
+//            approvalType: self.approvalType
+//          ) { result in
+//            self.clearInputs()
+//            self.transactionAdded = result
+//            self.showingAlert = true
+//          }
+//        }
+//      }.padding().alert(isPresented: $showingAlert) { () -> Alert in
+//        if self.transactionAdded {
+//          return Alert(title: Text("Transaction added"))
+//        } else {
+//          return Alert(title: Text("An error occured. Please try again."))
+//        }
+//      }
+//    }.background(Colors.aspireGray)
+//      .edgesIgnoringSafeArea(.all)
+//      .onTapGesture {
+//        UIApplication.shared.sendAction(
+//          #selector(UIResponder.resignFirstResponder),
+//          to: nil,
+//          from:
+//          nil,
+//          for: nil
+//        )
+//      }
   }
 }
 
