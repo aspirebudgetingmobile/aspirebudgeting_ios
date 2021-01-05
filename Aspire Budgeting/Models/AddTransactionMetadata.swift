8ad83c31bbbe0e077896fa35af954b070a30a7a4
Mohit Athwani <mohit.athwani@gmail.com>
2021-01-04 20:10:28 -0800

    Fetch Transaction Accounts and Categories (#128)

--- Aspire Budgeting/Models/AddTransactionMetadata.swift
+++ Aspire Budgeting/Models/AddTransactionMetadata.swift
@@ -5,7 +5,16 @@
 
 import Foundation
 
-struct AddTransactionMetadata {
+protocol ConstructableFromBatchRequest {
+  init(rowsList: [[String]])
+}
+
+struct AddTransactionMetadata: ConstructableFromBatchRequest {
   let transactionCategories: [String]
   let transactionAccounts: [String]
+
+  init(rowsList: [[String]]) {
+    transactionCategories = rowsList[0]
+    transactionAccounts = rowsList[1]
+  }
 }
