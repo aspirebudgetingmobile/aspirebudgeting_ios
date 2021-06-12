12ae8fb71425c5bf4278caa00a9aba347277aa8d
Mohit Athwani <mohit.athwani@gmail.com>
2021-06-12 15:14:57 -0700

    Linting

--- Aspire Budgeting/Views/ShareSheet.swift
+++ Aspire Budgeting/Views/ShareSheet.swift
@@ -7,8 +7,12 @@ import SwiftUI
 import UIKit
 
 struct ShareSheet: UIViewControllerRepresentable {
-    typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
-    
+    typealias Callback = (
+      _ activityType: UIActivity.ActivityType?,
+      _ completed: Bool,
+      _ returnedItems: [Any]?, _ error: Error?
+    ) -> Void
+
     let activityItems: [Any]
     let applicationActivities: [UIActivity]? = nil
     let excludedActivityTypes: [UIActivity.ActivityType]? = nil
