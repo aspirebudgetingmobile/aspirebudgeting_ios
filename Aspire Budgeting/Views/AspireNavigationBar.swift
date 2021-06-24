cfc483d91c982f1d9466511ba1d28843c6709ff2
Mohit Athwani <mohit.athwani@gmail.com>
2021-06-23 21:19:54 -0700

    Aligning the elements in the navigation bar

--- Aspire Budgeting/Views/AspireNavigationBar.swift
+++ Aspire Budgeting/Views/AspireNavigationBar.swift
@@ -15,6 +15,19 @@ struct AspireNavigationBar: View {
       Text(title)
         .font(.nunitoBold(size: 20))
         .foregroundColor(.primaryTextColor)
+        .alignmentGuide(.bottom, computeValue: { d in
+          d[.bottom] + (d.height / 4)
+        })
+
+      HStack(alignment: .center) {
+        Spacer()
+        Button(action: {}, label: {
+          Image(systemName: "repeat")
+        })
+        .alignmentGuide(VerticalAlignment.center, computeValue: { d in
+          d[.bottom] + (d.height / 1.5)
+        })
+      }.padding(.trailing)
     }
   }
 }
